e0a4924e02638c3ff3f42be56638391c0933a1df
Dicky Li <dickyli338@gmail.com>
2020-07-03 13:29:36 +0800

    initial commit with renderer

---
+++ lib/lad_view_tool.rb
@@ -0,0 +1,7 @@
+require "lad_view_tool/version"
+require "lad_view_tool/renderer"
+
+module LadViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lad_view_tool/version.rb
@@ -0,0 +1,3 @@
+module LadViewTool
+  VERSION = "0.1.0"
+end
